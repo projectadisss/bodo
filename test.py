539ae21800fac4531b9db751a4522dc2c8458cdf
projectadisss <buatdiscords99@gmail.com>
2026-02-23 16:16:23 +0700

    c

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ from SpoofedEmail import Email
 # Menyiapkan email dengan alamat pengirim palsu
 email = Email(
     sender="donotreply@register-sc.moonton.com",  # Pengirim palsu
-    recipient="bahanstoree@gmail.com",  # Penerima
+    recipient="aakhmadi699@gmail.com",  # Penerima
     subject="Mobile Legends: Bang Bang",  # Subjek
     message="This is a tes email, it appears to come from a different address."  # Isi email
 )
